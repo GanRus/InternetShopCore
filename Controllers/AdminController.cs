94acd1ed3b513925506b26a4c8ee80899f55bc08
Ganeev Ruslan <ganrus@rambler.ru>
2020-08-30 21:14:50 +0300

    Добавлена страница администрирования. Формы для добавления групп категорий товаров и категорий товаров

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetShopCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InternetShopCore.Controllers
 {
     public class AdminController : Controller
     {
+        private ShopContext db;
+
+        public AdminController(ShopContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult AddGroupCategory()
+        {
+            return View();
+        }
+
+        public IActionResult AddCategory()
+        {
+            ViewBag.GroupCategoryList = db.GroupCategories.ToList();
             return View();
         }
     }
